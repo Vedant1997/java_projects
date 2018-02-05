01b95703707a305c9dd5285ea5cb1a1b75314e28
Vedant1997 <vedants1997@gmail.com>
2018-02-05 13:49:52 +0530

    first commit

--- Project -1/src/File_listing_app.java
+++ Project -1/src/File_listing_app.java
@@ -1,54 +1,62 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class File_listing_app {
-	public static void expand(String directory_to_serach,PrintStream writer) {
-		File directory =new File(directory_to_serach);
+	public static void expand(File directory,ArrayList al) {       										//This function is used to explore the directory  
+		
 		File files[]=directory.listFiles();
+		
 		for(int a=0;a<files.length;a++) {
 			if(files[a].isFile()) {
-				writer.println("Name :"+files[a].getName()+"-------------------------- Location:"+files[a].getAbsolutePath());
+				al.add(files[a].getName()+","+files[a].getAbsolutePath());
 			}
 			if(files[a].isDirectory()) {
-				writer.println("Name :"+files[a].getName()+"-------------------------- Location:"+files[a].getAbsolutePath());
-				expand(files[a].getName(),writer);
+				al.add(files[a].getName()+","+files[a].getAbsolutePath());
+				expand(files[a],al);
 			}
+		
 		}	 
 	}
 	
 	public static void main(String[] args) {
-		Scanner scan =new Scanner(System.in); 
-		System.out.println("Enter the Location of the file :");
-		String location=scan.nextLine();
-		File file=new File(location);
-		String directory_to_serach="";
-		String csv_file="";
-
 		try { 
+			Scanner scan =new Scanner(System.in);  
+			System.out.println("Enter the Location of the file :");
+			String location=scan.nextLine();
+			File file=new File(location);																	  //This is the text file which contains the two urls
+			String directory_to_search="";
+			String csv_file=""; 
 			FileReader in = new FileReader(file);
 			int current_char ;
 				  while((current_char =  in.read())!=10) {
 					   char ch = (char)current_char;
-					   directory_to_serach = directory_to_serach + ch; 
+					   directory_to_search = directory_to_search + ch; 
 				  }
-				  File dir =new File(directory_to_serach);
+	
+				  File dir =new File(directory_to_search.substring(0, directory_to_search.length()-1));   //This is the directory which have been provided by the file
 				  while((current_char =  in.read())!=-1) {
 					   char ch = (char)current_char;
-					   csv_file = csv_file + ch;   
-				  }
-				  
-				  System.out.println("csv file :"+csv_file);
-				  System.out.println("directory file :"+directory_to_serach);
-				  File out =new File(csv_file);
-				  PrintStream writer =new PrintStream(out);
-				  expand(directory_to_serach.substring(0, directory_to_serach.length()-1),writer);
+					   csv_file = csv_file + ch;    
+				  } 
+				  File output = new File(csv_file);
+				  ArrayList al=new ArrayList();
+				  PrintStream writer =new PrintStream(output); 
+				  expand(dir,al);																		//Here I recieved the arraylist which has been edited by the 
+				  																						//expand method
+				  Iterator irt =al.iterator();
+					while(irt.hasNext()) {
+						writer.println((String)irt.next());
+					}
 				  
 		} catch (Exception e) {
-			e.printStackTrace(); 
+			System.out.println("The location you have entered doesn't have the the file mentioned");  
 		}
 		
 	}
